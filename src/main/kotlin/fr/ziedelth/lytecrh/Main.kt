f5ba443e74f1ef25cabebef2666a38c73a33d657
Ziedelth <watteaupierre@gmail.com>
2023-01-18 22:41:55 +0100

    Change print and remove h265 codec

--- src/main/kotlin/fr/ziedelth/lytecrh/Main.kt
+++ src/main/kotlin/fr/ziedelth/lytecrh/Main.kt
@@ -8,10 +8,10 @@ import net.bramp.ffmpeg.builder.FFmpegBuilder
 import net.bramp.ffmpeg.probe.FFmpegProbeResult
 import net.bramp.ffmpeg.progress.Progress
 import java.util.concurrent.TimeUnit
+import kotlin.math.abs
 
-private const val CRF = 28.0
+private const val CRF = 30.0
 private const val VBR = 4L
-private const val VIDEO_PRESET = "veryfast"
 
 private fun toH264(fFmpegProbeResult: FFmpegProbeResult): FFmpegBuilder {
     return FFmpegBuilder()
@@ -22,7 +22,6 @@ private fun toH264(fFmpegProbeResult: FFmpegProbeResult): FFmpegBuilder {
         .setVideoCodec("libx264")
         .setConstantRateFactor(CRF)
         .setVideoPixelFormat("yuv420p")
-        .addExtraArgs("-preset:v", VIDEO_PRESET)
         .addExtraArgs("-profile:v", "baseline")
         .addExtraArgs("-level", "3.0")
         .setVideoBitRate(VBR)
@@ -30,35 +29,7 @@ private fun toH264(fFmpegProbeResult: FFmpegProbeResult): FFmpegBuilder {
         .done()
 }
 
-private fun toH265(fFmpegProbeResult: FFmpegProbeResult): FFmpegBuilder {
-    return FFmpegBuilder()
-        .setInput(fFmpegProbeResult)
-        .overrideOutputFiles(true)
-        .addOutput("output.mp4")
-        .setFormat("mp4")
-        .setVideoCodec("libx265")
-        .setVideoPixelFormat("yuv420p")
-        .addExtraArgs("-preset:v", VIDEO_PRESET)
-        .addExtraArgs("-x265-params", "crf=${CRF.toInt()}")
-        .setVideoBitRate(VBR)
-        .setAudioCodec("copy")
-        .done()
-}
-
-private fun toVP9(fFmpegProbeResult: FFmpegProbeResult): FFmpegBuilder {
-    return FFmpegBuilder()
-        .setInput(fFmpegProbeResult)
-        .overrideOutputFiles(true)
-        .addOutput("output.mp4")
-        .setFormat("mp4")
-        .setVideoCodec("libvpx-vp9")
-        .setVideoPixelFormat("yuv420p")
-        .addExtraArgs("-preset:v", VIDEO_PRESET)
-        .addExtraArgs("-crf", CRF.toString())
-        .setVideoBitRate(VBR)
-        .setAudioCodec("copy")
-        .done()
-}
+private fun Double.toSign(): String = if (this > 0) String.format("-%.2f", this) else String.format("+%.2f", abs(this))
 
 fun main() {
     val fFmpeg = FFmpeg()
@@ -79,7 +50,7 @@ fun main() {
 
         if (progressInSec != lastSecond) {
             val currentTime = System.currentTimeMillis()
-            val time = currentTime - lastTime
+            val time = (currentTime - lastTime) / abs(progressInSec - lastSecond)
 
             if (times.size > 100) {
                 times.removeAt(0)
@@ -94,20 +65,25 @@ fun main() {
         val diffSeconds = fFmpegProbeResult.format.duration - progressInSec
         val remainingTime = diffSeconds * times.average()
 
-        println(String.format(
-            "[%.0f%%] Current time encoded: %s - Speed: %.2fx - Remaining: %s",
-            percentage,
-            FFmpegUtils.toTimecode(progress.out_time_ns, TimeUnit.NANOSECONDS).split(".")[0],
-            progress.speed,
-            FFmpegUtils.toTimecode(remainingTime.toLong(), TimeUnit.MILLISECONDS).split(".")[0]
-        ))
+        print("\b".repeat(100))
+        print(
+            String.format(
+                "%s\t%s\t%.2f%%\t%s",
+                FFmpegUtils.toTimecode(progress.out_time_ns, TimeUnit.NANOSECONDS).split(".")[0],
+                "[${"#".repeat((percentage / 2).toInt())}${" ".repeat((50 - percentage / 2).toInt())}]",
+                percentage,
+                FFmpegUtils.toTimecode(remainingTime.toLong(), TimeUnit.MILLISECONDS).split(".")[0]
+            )
+        )
 
         if (progress.status == Progress.Status.END) {
             val toDouble = fFmpegProbeResult.format.size.toDouble()
             val diff = (toDouble - progress.total_size.toDouble()) / toDouble * 100.0
 
+            println(System.lineSeparator())
+            println("Original size: ${String.format("%.2f", toDouble / (1024 * 1024))} MB - New size: ${String.format("%.2f", progress.total_size.toDouble() / (1024 * 1024))} MB")
+            println("Compression ratio: ${diff.toSign()}%")
             println()
-            println("Difference: ${String.format("%.2f", diff)}%")
             println("Total taken time: ${String.format("%.2f", (System.currentTimeMillis() - start) / 60000.0)} min")
         }
     }.run()
